5bdb29788b9d74412962441a73de7db29cae48e9
leschekhomann <26705442+leschekhomann@users.noreply.github.com>
2024-06-09 21:35:23 +0200

    Update qemu.rb

--- qemu.rb
+++ qemu.rb
@@ -17,75 +17,7 @@ class Qemu < Formula
     sha256 x86_64_linux:   "05ece9acbbb003a7e8035f18f5193ae7c9caffacd60a744eac629de4a545aea4"
   end
 
-  depends_on "libtool" => :build
-  depends_on "meson" => :build
-  depends_on "ninja" => :build
-  depends_on "pkg-config" => :build
-
-  depends_on "capstone"
-  depends_on "glib"
-  depends_on "gnutls"
-  depends_on "jpeg-turbo"
-  depends_on "libpng"
-  depends_on "libslirp"
-  depends_on "libssh"
-  depends_on "libusb"
-  depends_on "lzo"
-  depends_on "ncurses"
-  depends_on "nettle"
-  depends_on "pixman"
-  depends_on "snappy"
-  depends_on "vde"
-  depends_on "zstd"
-
-  on_linux do
-    depends_on "attr"
-    depends_on "gtk+3"
-    depends_on "libcap-ng"
-  end
-
-  fails_with gcc: "5"
-
-  # 820KB floppy disk image file of FreeDOS 1.2, used to test QEMU
-  resource "homebrew-test-image" do
-    url "https://www.ibiblio.org/pub/micro/pc-stuff/freedos/files/distributions/1.2/official/FD12FLOPPY.zip"
-    sha256 "81237c7b42dc0ffc8b32a2f5734e3480a3f9a470c50c14a9c4576a2561a35807"
-  end
-
   def install
-    ENV["LIBTOOL"] = "glibtool"
-
-    args = %W[
-      --prefix=#{prefix}
-      --cc=#{ENV.cc}
-      --host-cc=#{ENV.cc}
-      --disable-bsd-user
-      --disable-guest-agent
-      --enable-slirp
-      --enable-capstone
-      --enable-curses
-      --enable-libssh
-      --enable-vde
-      --enable-virtfs
-      --enable-zstd
-      --extra-cflags=-DNCURSES_WIDECHAR=1
-      --disable-sdl
-    ]
-
-    # Sharing Samba directories in QEMU requires the samba.org smbd which is
-    # incompatible with the macOS-provided version. This will lead to
-    # silent runtime failures, so we set it to a Homebrew path in order to
-    # obtain sensible runtime errors. This will also be compatible with
-    # Samba installations from external taps.
-    args << "--smbd=#{HOMEBREW_PREFIX}/sbin/samba-dot-org-smbd"
-
-    args += if OS.mac?
-      ["--disable-gtk", "--enable-cocoa"]
-    else
-      ["--enable-gtk"]
-    end
-
-    # system "./configure", *args
     system "./configure"
     system "make", "install"
   end
